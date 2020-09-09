9f04b39920f2b3459534ace0927aa38677f92b2b
Daniel Lee <daniejlee@gmail.com>
2020-09-08 21:33:53 -0700

    obj manipulation 2

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+const pick = (source, keys) => {
+  const newObj = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (Object.prototype.hasOwnProperty.call(source, keys[i]) && source[keys[i]] !== undefined) {
+      newObj[keys[i]] = source[keys[i]];
+    }
+  }
+  return newObj;
+};
