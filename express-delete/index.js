d3bcc68876f9c4bfdcf68baf7ca553f090cc3194
Daniel Lee <daniejlee@gmail.com>
2020-09-10 11:50:01 -0700

    express delete

---
+++ express-delete/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console, no-unused-vars */
+const express = require('express');
+const app = express();
+
+const grades = [
+  {
+    id: 1,
+    name: 'dan',
+    course: 'nope',
+    grade: '7'
+  },
+  {
+    id: 2,
+    name: 'rich',
+    course: 'golf',
+    grade: '6'
+  }
+];
+
+app.get('/api/grades', (req, res) => {
+  res.send(grades);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const arrIndex = grades.find(key => key.id === req.params.id);
+  grades.splice(arrIndex, 1);
+  res.send('Entry Deleted');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on 3000');
+});
