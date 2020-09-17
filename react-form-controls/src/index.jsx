10b54402e0b31d3c13c01b3d1f08f0af6b2dd732
Daniel Lee <daniejlee@gmail.com>
2020-09-17 15:42:41 -0700

    react form controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label> Email
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <button type="submit" value="submit">Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.querySelector('#root')
+);
